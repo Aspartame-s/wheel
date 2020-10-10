3361df35883c408a2324ad5fd3bca33abed940fd
jth <327127960@qq.com>
2020-10-10 18:38:55 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,8 @@
 <template>
      <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
         
-        <g-icon v-if="icon" :name="icon"></g-icon>
+        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
        <div class="content">
             <slot></slot>
        </div>
@@ -21,6 +22,10 @@
 </script>
 
 <style lang="scss">
+    @keyframes spin {
+        0%{transform: rotate(0deg);}
+        100%{transform: rotate(360deg);}
+    }
      .g-button {
             font-size: var(--font-size);
             height: var( --button-height);
@@ -33,7 +38,6 @@
             justify-content: center;
             align-items: center;
             vertical-align: middle;
-            padding: 0 1em;
         &:hover {
             border-color: var(--border-hover-color);
         }
@@ -51,6 +55,9 @@
         &.icon-right {
             > .content {order: 1}
             > .icon {order: 2; margin-right: 0; margin-left: .1em;} 
-        }   
+        }
+        .loading {
+            animation: spin 2s infinite linear;
+        }  
     }     
 </style>
